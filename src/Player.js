c46c2689305cb12d8acdf1e728ad49a561e7ee40
Ashish Singh <ashishsingh.1899@gmail.com>
2023-03-17 19:56:34 +0530

    initial commit

---
+++ src/Player.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Player.css"
+function Player() {
+  return (
+      <div className='player'>
+          <div className="player__body">
+              {/* Sidebar */}
+              {/* Body */}
+          </div>
+          <div>
+                {/* Footer */}
+          </div>
+    </div>
+  )
+}
+
+export default Player
